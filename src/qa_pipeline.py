598ab505289ec59bd80d177668f233b2014cf28f
ankur3107 <ankur310794@gmail.com>
2021-07-24 22:52:52 +0530

    qa bug fixed

--- src/qa_pipeline.py
+++ src/qa_pipeline.py
@@ -1,4 +1,4 @@
-from utils import DataProcessor DataReader DataReranker DataRetriever
+from utils import DataProcessor, DataReader, DataReranker, DataRetriever
 
 class Pipeline():
 
@@ -6,14 +6,14 @@ class Pipeline():
         self.data_processor = DataProcessor()
         self.data_processor.initialize_temp_store()
 
-        self.data_retriever = DataRetriever(data_processor.document_store)
-        self.data_processor.update_embeddings(data_retriever.retriever)
+        self.data_retriever = DataRetriever(self.data_processor.document_store)
+        self.data_processor.update_embeddings(self.data_retriever.retriever)
 
         self.data_reranker = DataReranker()
         self.data_reader = DataReader()
 
-    def run(queries, top_k_retrieve=5, top_k_ranker=3):
+    def run(self, queries, top_k_retrieve=5, top_k_ranker=3):
         retrieve_documents = self.data_retriever.batch_retrieve(queries, top_k_retrieve, index='document')
-        reranked_documents = data_reranker.run(queries, retrieve_documents, top_k_ranker = top_k_ranker, top_k_retrieve = top_k_retrieve)
+        reranked_documents = self.data_reranker.run(queries, retrieve_documents, top_k_ranker = top_k_ranker, top_k_retrieve = top_k_retrieve)
         results = self.data_reader.run(queries, reranked_documents)
         return results
